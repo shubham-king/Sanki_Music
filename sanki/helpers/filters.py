ee7fb66da3487b5862d3a74641063b2fa56d54e2
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-09-03 12:37:35 +0530

    Update filters.py

--- sanki/helpers/filters.py
+++ sanki/helpers/filters.py
@@ -19,7 +19,7 @@ from typing import List, Union
 
 from pyrogram import filters
 
-from MusicKen.config import COMMAND_PREFIXES
+from Sanki_Music.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
